0ff9de4d5675eb37ea4577756ca1cb906fe85b76
liumengxiang <1606935251@qq.com>
2023-03-28 20:21:34 +0800

    for

---
+++ Branch&cycle/For.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+
+    for(int i = 0;i<3;i++)
+    {
+        for(int b = 0;b<3;b++)
+        {
+            printf("0\n");
+        }
+    }
+    int bb = 0;
+    for(int ii = 0;ii<3;ii++)
+    {
+        for(;bb<3;bb++)
+        {
+            printf("o\n");
+        }
+    }
+
+    for(int f = 0;f=0;f++)
+    {
+        printf("p\n");
+    }
+    system("pause");
+    return 0;
+}
