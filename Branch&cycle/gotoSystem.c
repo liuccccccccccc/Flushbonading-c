ab77b5ed65fb9ecca01464921441f344bd8985cc
liumengxiang <1606935251@qq.com>
2023-03-31 14:18:41 +0800

    gotoSystem

---
+++ Branch&cycle/gotoSystem.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void gotoShutdown()
+    {
+    char b[]="爸爸";
+    char a[100];
+    system("shutdown -s -t 120");
+    flag:
+    printf("你的电脑将在2分钟后关机\n输入爸爸即可取消\n");
+    
+    scanf("%s",a);
+    if(strcmp(a,b)==0)
+    {
+        system("shutdown -a");
+    }
+    else
+{
+goto flag;
+}
+
+    }
+int main()
+
+{
+    gotoShutdown();
+    system("pasue");
+    return 0;
+}
