27c5fe2995d075726c9a986afa568aa72b3e5aaf
liumengxiang <1606935251@qq.com>
2023-04-01 20:17:36 +0800

    C_project_Model

---
+++ C_Project_Model/FunctionFile.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+int Add(int a,int b)
+{
+    return a+b;
+}
+
+//函数文件，这个文件是用来专门写各种功能函数。
---
+++ C_Project_Model/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"FunctionFile.h"//引用.h声名文件，引用后便可调用文件中的函数。
+int main()
+{
+
+    printf("%d",Add(3,4));
+    system("pause");
+    return 0;
+}
