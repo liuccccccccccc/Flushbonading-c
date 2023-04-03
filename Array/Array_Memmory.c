4bde38e3d86ae109b601e6a9618da43478a5249c
liumengxiang <1606935251@qq.com>
2023-04-03 19:59:56 +0800

    数组内存

---
+++ Array/Array_Memmory.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+ int arr[10] = {0};
+ int i = 0;
+ int sz = sizeof(arr)/sizeof(arr[0]);
+ for(i=0; i<sz; ++i)
+ {
+ printf("&arr[%d] = %p\n", i, &arr[i]);
+ }
+ printf("%p",arr);//数组的首地址就是一个指针，存放的是第一个元素的地址，arr=&arr[0];
+ system("pause");
+ return 0;
+}
