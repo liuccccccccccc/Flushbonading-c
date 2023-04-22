6327a641e5a81563f19a1eff74530904d97a92f5
liumengxiang <1606935251@qq.com>
2023-04-22 22:17:08 +0800

    计算阶乘

--- Branch&cycle/ComputeFactorial.c
+++ Branch&cycle/ComputeFactorial.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 int Factorial(int n)
 {
     int Value = 0;
     int i = n;
-    for(Value = n - 1;Value >= 1;Value--)
+    for (Value = n - 1; Value >= 1; Value--)
     {
         i *= Value;
     }
@@ -15,10 +15,9 @@ int main()
 {
     int a;
     printf("请输入你要哪个数的阶乘:");
-    scanf("%d",&a);
-    int FactValue=Factorial(a);
-    printf("%d!=%d\n",a,FactValue);
+    scanf("%d", &a);
+    int FactValue = Factorial(a);
+    printf("%d!=%d\n", a, FactValue);
     system("pause");
     return 0;
-
 }
