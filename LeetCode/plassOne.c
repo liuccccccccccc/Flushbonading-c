8c59bf406d3b69bf8839593c3f9119b5538459aa
liumengxiang <1606935251@qq.com>
2023-07-19 21:28:07 +0800

    加1

--- LeetCode/plassOne.c
+++ LeetCode/plassOne.c
@@ -30,7 +30,8 @@ int *plusOne(int *digits, int digitsSize, int *returnSize)
     // }
     // *plusArr = temp;
     // return plusArr;
-    if (digits[digitsSize - 1] >= 0 && digits[digitsSize - 1] <= 8)
+    //方法二：通过研究数组的每一位数来找到新数组
+    if (digits[digitsSize - 1] >= 0 && digits[digitsSize - 1] <= 8)//先判断最后一位是否为9，如果不为9直接开辟同等长度数组，最后一个数加1，其他数原样拷贝
     {
         int *returnArr = malloc(digitsSize * 4);
         *returnSize = digitsSize;
@@ -41,10 +42,10 @@ int *plusOne(int *digits, int digitsSize, int *returnSize)
         *(returnArr + digitsSize - 1) = *(digits + digitsSize - 1) + 1;
         return returnArr;
     }
-    else
+    else//如果最后一位为9那么再判断全部数是否为9
     {
         int flag = 1;
-        for (int j = digitsSize - 1; j >= 0; j--)
+        for (int j = digitsSize - 1; j >= 0; j--)//利用flag来判断数是否全部为9，如果遍历一遍flag的值为1，那么说明flag的值未变，说明数全为1，否则一旦flag变为0，就说明有不为9的数，此时不全为9
         {
             if (digits[j] == 9 && flag == 1)
             {
@@ -55,7 +56,7 @@ int *plusOne(int *digits, int digitsSize, int *returnSize)
                 flag = 0;
             }
         }
-        if (flag == 1)
+        if (flag == 1)//如果全部数字都为9，那么开辟数组长度加1，，第一位为1其他位补零。
         {
             int *returnArr = malloc((digitsSize + 1) * 4);
             for (int i = 1; i < (digitsSize + 1); i++)
@@ -66,11 +67,11 @@ int *plusOne(int *digits, int digitsSize, int *returnSize)
             *returnSize = digitsSize + 1;
             return returnArr;
         }
-        else
+        else//如果不全为9，数组长度就开辟和原来数组一样的长度。
         {
             int *returnArr = malloc(digitsSize * 4);
             int buffer = 1;
-            for (int i = digitsSize - 1; i >= 0; i--)
+            for (int i = digitsSize - 1; i >= 0; i--)//利用buffer来对加1后的数组的每一为的值进行计算
             {
                 if (buffer == 1 && digits[i] == 9)
                 {
