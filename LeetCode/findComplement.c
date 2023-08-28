7a1aa18665413054ef02b3a22ba56ef0974dfc87
liumengxiang <1606935251@qq.com>
2023-08-28 21:49:44 +0800

    求补数

---
+++ LeetCode/findComplement.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int findComplement(int num){
+    int a,b,sum1=0,sum2=0,count=0;
+    while(1)
+    {
+        b=num&1;
+        a=!b;
+        sum1+=a*pow(2,count);
+        sum2+=b*pow(2,count);
+        count++;
+        num=num>>1;
+        if(sum2==num)
+        {
+            break;
+        }
+    }
+    return sum1;
+
+}
+int main()
+{
+    int a=findComplement(1);
+    printf("%d\n",a);
+    system("pause");
+    return 0;
+}
