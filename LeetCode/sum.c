807bd7f239a38109d8f0d615eb4afcf3db62d4a0
liumengxiang <1606935251@qq.com>
2024-01-09 19:36:30 +0800

    两数之和

---
+++ LeetCode/sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int sum(int num1, int num2){//2235
+    return num1+num2;
+}
+int main()
+{
+    printf("%d\n",sum(-100,101));
+    system("pause");
+    return 0;
+}
